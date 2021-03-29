47620bdcb21c86b9ddb7bfa3a48cf42a69438362
Bebe Lawrence <omlawrence24@gmail.com>
2021-03-28 20:48:00 -0400

    db update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const horoscopeRoutes = require("./horoscopeRoutes");
-const signRoutes = require("./signRoutes")
+// const horoscopeRoutes = require("./horoscopeRoutes");
+// const signRoutes = require("./signRoutes")
 
 router.use("/users", userRoutes);
 // router.use("/horoscopes", horoscopeRoutes);
