2563c2add117d8e778ca59b346edcc47a15826ab
Courtney Fox <courtney.fox@me.com>
2021-04-01 22:50:40 -0400

    Use EST date

--- utils/dateFormatter.js
+++ utils/dateFormatter.js
@@ -1,7 +1,9 @@
 module.exports = (date) => {
-    const year = date.getFullYear();
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const day = date.getDate();
+    const estDate = new Date(date.toLocaleString('en-US', { timeZone: 'America/New_York' }));
+    console.log("estDate", estDate);
+    const year = estDate.getFullYear();
+    const month = ("0" + (estDate.getMonth() + 1)).slice(-2);
+    const day = estDate.getDate();
 
-    return `${year}-${month}-${day}`
+    return `${year}-${month}-${day}`;
 }
